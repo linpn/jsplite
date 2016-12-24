dacb42e690672fcbdc9c4ab624dfb83288cb2340
pinian.lpn <pinian.lpn@alibaba-inc.com>
2016-12-24 21:13:56 +0800

    A simple jsp mvc

--- mvn-repository/github-deply.bat
+++
@@ -1,4 +0,0 @@
-del com/
-cd ../
-call mvn -DaltDeploymentRepository=snapshot-repo::default::file:mvn-repository clean deploy
-@pause
--- mvn-repository/github-deply.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf com/
-cd ../
-mvn -DaltDeploymentRepository=snapshot-repo::default::file:mvn-repository clean deploy
